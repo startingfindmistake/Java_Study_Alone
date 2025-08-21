4f27f0e05a441c41694d0e9ee8ecffd0b004ba0d
Startingfindmistakesamsung <dudgh2328@gmail.com>
2025-08-21 20:50:43 +0900

    update

---
+++ chpater1/Hello.java
@@ -0,0 +1,16 @@
+package chpater1;
+
+/*
+ * 작성자: startingfindmistake
+ * 작성일: 2025-08-21
+ * 내용: Hello World
+ */
+
+ public class Hello {
+ 
+    // 프로그램 실행 진입점
+    public static void main(String[] args) {
+        // 콘솔에 출력하는 실행문
+        System.out.println("Hello, Java");
+    } // end of main
+ } // end of class
---
+++ chpater1/VariableInitializationExample.java
@@ -0,0 +1,20 @@
+// 변수 초기화
+// 오류가 나는 코드 입니다.(오류테스트 코드)
+package chpater1;
+
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+        // 변수 value 선언
+        int value;
+
+        // 변수 value 값을 읽고 10을 더하는 산술 연산을 수행
+        // 연산의 결과값을 변수 result의 초기값으로 저장
+        int result = value + 10;
+        
+        // 콘솔에 출력하는 실행문
+        //System.out.println("result: " + result);
+
+        // 변수 result 값을 읽고 콘솔에 출력
+        System.out.println(result);
+    }
+}
