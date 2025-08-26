54a2d586cd9bb3e24eab158d4fdc5fb77159a2ec
Startingfindmistakesamsung <dudgh2328@gmail.com>
2025-08-26 21:20:02 +0900

    update

---
+++ chapter6/chapter6_1/StudentExample.java
@@ -0,0 +1,12 @@
+package chapter6.chapter6_1;
+
+public class StudentExample {
+    public static void main(String[] args) {
+        Studnet s1 = new Studnet();
+        System.out.println("s1 변수가 Studnet 객체를 참조합니다.");
+
+        Studnet s2 = new Studnet();
+        System.out.println("s2 변수가 또 다른 Student 객체를 참조합니다.");
+    }
+    
+}
